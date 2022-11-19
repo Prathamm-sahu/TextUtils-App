8e12bbe18039c01f4715d2da6c64b1f0f9898005
Pratham <prathamsahu051@gmail.com>
2022-11-19 15:40:29 +0530

    added word count and preview feature

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,13 +6,18 @@ export default function TextForm(props) {
         let newText = text.toUpperCase();
         setText(newText);
     }
+    const handleLoClick = () => {
+        console.log("Lowercase was clicked" + text)
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
     
     const handleOnChange = (event) => {
         console.log("On change");
         setText(event.target.value);
     }
     
-    const [text, setText] = useState("Enter text Here");
+    const [text, setText] = useState("");
     // text = "new text"  // Wrong Way to change the state
     // setText("new text")   // correct Way to change the state
      return (
@@ -22,8 +27,17 @@ export default function TextForm(props) {
                 <div className="mb-3">
                   <textarea id="myBox" cols="100" rows="8" value={text} onChange={handleOnChange}></textarea>  
                 </div>
-                <button type="submit" className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+                <button type="submit" className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+                <button type="submit" className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
             </form>
+
+            <div className="container my-3">
+                <h3>Your text summary</h3>
+                <p>{text.split(" ").length -1} words and {text.length} characters</p>
+                <p>{0.008* text.split(" ").length} Minutes read</p>
+                <h3>Preview</h3>
+                <p>{text}</p>
+            </div>
         </>
     )
 }
