37e21ef7bee5b4cef466739f2f16941e37ffba6e
Pratham <prathamsahu051@gmail.com>
2022-11-19 21:16:07 +0530

    Added some buttons and alert

--- src/App.js
+++ src/App.js
@@ -1,16 +1,49 @@
 // import logo from './logo.svg';
+import react, {useState} from 'react';
 import './App.css';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import Alert from './components/Alert';
 
 
 
 function App() {
+  const [mode, setMode] = useState("light");  // Whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null)
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type 
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 3000)
+  }
+
+  const toggleMode = () => {
+    if(mode === "light") {
+      setMode("dark")
+      document.body.style.backgroundColor = "#072039"
+      document.body.style.color = "white"
+      showAlert("Dark mode has been enabled", "success")
+    }
+    else {
+      setMode("light")
+      document.body.style.backgroundColor = "white"
+      document.body.style.color = "black"
+      showAlert("Light mode has been enabled", "success")
+    }
+  }
   return (
     <>
-      <Navbar title="Textutils" aboutText="About Textutils" />
+      {/* <Navbar title="Textutils" aboutText="About Textutils"/> */}
+      <Navbar title="Textutils" mode={mode} toggleMode={toggleMode}/>
+      <Alert alert={alert}/>
       <div className="container my-3">
-        <TextForm heading="Enter the text to analyze" />
+        <TextForm heading="Enter the text to analyze" mode={mode}/>
+        {/* <About/> */}
       </div>
       
 
