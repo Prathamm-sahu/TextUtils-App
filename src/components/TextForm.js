453a6c93a3f3ede79db9fdcff08dc85146bea4aa
Pratham <prathamsahu051@gmail.com>
2022-11-19 14:36:40 +0530

    Added Convert to Uppercasse feature

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,24 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 export default function TextForm(props) {
-    return (
+    
+    const handleUpClick = () => {
+        console.log("Uppercase was clicked" + text)
+        let newText = text.toUpperCase();
+        setText(newText);
+    }
+    
+    const handleOnChange = (event) => {
+        console.log("On change");
+        setText(event.target.value);
+    }
+    
+    const [text, setText] = useState("Enter text Here");
+    // text = "new text"  // Wrong Way to change the state
+    // setText("new text")   // correct Way to change the state
+     return (
         <>
-
             <h2>{props.heading}</h2>
             <form>
                 <div className="mb-3">
-                    <label for="exampleInputEmail1" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-                        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                  <textarea id="myBox" cols="100" rows="8" value={text} onChange={handleOnChange}></textarea>  
                 </div>
-                <div className="mb-3">
-                    <label for="exampleInputPassword1" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1"/>
-                </div>
-                <div className="mb-3 form-check">
-                    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-                        <label className="form-check-label" for="exampleCheck1">Check me out</label>
-                </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
+                <button type="submit" className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
             </form>
         </>
     )
